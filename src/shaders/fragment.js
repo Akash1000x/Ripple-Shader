83078be7d4ecdb5f3e0fc096215b22b31d609689
Akash kumar kumawat <kumawatap63@gmail.com>
2024-08-03 00:29:28 +0530

    update fragment

--- src/shaders/fragment.js
+++ src/shaders/fragment.js
@@ -1,21 +1,18 @@
 export const fragment = `
-uniform float time;
-uniform float progress;
 uniform sampler2D uTexture;
 uniform sampler2D uDisplacement;
-uniform vec4 resolution;
+uniform vec4 winResolution;
 varying vec2 vUv;
-varying vec3 vPosition;
 float PI = 3.141592653589793238;
 
 void main() {
-  vec4 displacement = texture2D(uDisplacement, vUv);
+  vec2 vUvScreen = gl_FragCoord.xy / winResolution.xy;
+
+  vec4 displacement = texture2D(uDisplacement, vUvScreen);
   float theta = displacement.r*2.0*PI;
 
   vec2 dir = vec2(sin(theta),cos(theta));
-
-  vec2 uv = vUv + dir*displacement.r*0.1;
-
+  vec2 uv = vUvScreen + dir*displacement.r*0.075;
   vec4 color = texture2D(uTexture,uv);
 
   gl_FragColor = color;
