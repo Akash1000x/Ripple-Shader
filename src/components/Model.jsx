00bd19830db19bffe92d8bb9e38e654cf4de71a5
Akash kumar kumawat <kumawatap63@gmail.com>
2024-08-04 21:43:44 +0530

    fix image texture

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -17,7 +17,9 @@ export default function Model() {
   const device = useDimension();
   const [prevMouse, setPrevMouse] = useState({ x: 0, y: 0 });
   const [currentWave, setCurrentWave] = useState(0);
-  const { gl, scene, camera } = useThree();
+  const { gl, camera } = useThree();
+
+  const scene = new THREE.Scene();
   const max = 100;
 
   const uniforms = useRef({
@@ -30,6 +32,8 @@ export default function Model() {
 
   const fboBase = useFBO(device.width, device.height);
   const fboTexture = useFBO(device.width, device.height);
+
+  const { scene: imageScene, camera: imageCamera } = Images(viewport);
 
   useEffect(() => {
     const generatedMeshes = Array.from({ length: max }).map((_, i) => (
@@ -67,7 +71,7 @@ export default function Model() {
     setPrevMouse({ x: x, y: y });
   }
 
-  useFrame(() => {
+  useFrame(({ gl, scene: finalScene }) => {
     const x = mouse.x - device.width / 2;
     const y = -mouse.y + device.height / 2;
     trackMousePos(x, y);
@@ -97,10 +101,14 @@ export default function Model() {
           scene.remove(mesh);
         }
       });
-      uniforms.current.uDisplacement.value = fboBase.texture;
-      uniforms.current.uTexture.value = fboBase.texture;
-      gl.setRenderTarget(null);
+      uniforms.current.uTexture.value = fboTexture.texture;
 
+      gl.setRenderTarget(fboTexture);
+      gl.render(imageScene, imageCamera);
+      uniforms.current.uDisplacement.value = fboBase.texture;
+
+      gl.setRenderTarget(null);
+      gl.render(finalScene, camera);
       // Render the scene with updated displacement
       // gl.setRenderTarget(fboTexture);
       // gl.clear();
@@ -112,43 +120,64 @@ export default function Model() {
         device.pixelRatio
       );
     }
-  });
+  }, 1);
 
-  function Images() {
-    const { viewport } = useThree();
-
-    return (
-      <group>
-        <Image
-          position={[-0.25 * viewport.width, 0, 1]}
-          scale={[viewport.width / 5, viewport.width / 4, 1]}
-          url="/images/picture1.jpeg"
-          alt="Picture 1"
-        />
-        <Image
-          position={[0 * viewport.width, 0, 1]}
-          scale={[viewport.width / 5, viewport.width / 4, 1]}
-          url="/images/picture2.jpeg"
-          alt="Picture 2"
-        />
-        <Image
-          position={[0.25 * viewport.width, 0, 1]}
-          scale={[viewport.width / 5, viewport.width / 4, 1]}
-          url="/images/picture3.jpeg"
-          alt="Picture 3"
-        />
-      </group>
+  function Images(viewport) {
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(
+      viewport.width / -2,
+      viewport.width / 2,
+      viewport.height / 2,
+      viewport.height / -2,
+      -1000,
+      1000
     );
+    camera.position.z = 2;
+    scene.add(camera);
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const group = new THREE.Group();
+    const texture1 = useTexture("/images/picture1.jpeg");
+    const material1 = new THREE.MeshBasicMaterial({ map: texture1 });
+    const image1 = new THREE.Mesh(geometry, material1);
+    image1.position.x = -0.25 * viewport.width;
+    image1.position.y = 0;
+    image1.position.z = 1;
+    image1.scale.x = viewport.width / 5;
+    image1.scale.y = viewport.width / 4;
+    group.add(image1);
+
+    const texture2 = useTexture("/images/picture2.jpeg");
+    const material2 = new THREE.MeshBasicMaterial({ map: texture2 });
+    const image2 = new THREE.Mesh(geometry, material2);
+    image2.position.x = 0;
+    image2.position.y = 0;
+    image2.position.z = 1;
+    image2.scale.x = viewport.width / 5;
+    image2.scale.y = viewport.width / 4;
+    group.add(image2);
+
+    const texture3 = useTexture("/images/picture3.jpeg");
+    const material3 = new THREE.MeshBasicMaterial({ map: texture3 });
+    const image3 = new THREE.Mesh(geometry, material3);
+    image3.position.x = 0.25 * viewport.width;
+    image3.position.y = 0;
+    image3.position.z = 1;
+    image3.scale.x = viewport.width / 5;
+    image3.scale.y = viewport.width / 4;
+    group.add(image3);
+
+    scene.add(group);
+    return { scene, camera };
   }
 
   return (
     <group>
       {meshes}
+      {/* <Images /> */}
       <mesh>
-        <Images />
         <planeGeometry args={[device.width, device.height, 1, 1]} />
         <shaderMaterial
-          args={[device.width / 2, device.height / 2]}
+          // args={[device.width, device.height, 1]}
           vertexShader={vertex}
           fragmentShader={fragment}
           transparent={true}
